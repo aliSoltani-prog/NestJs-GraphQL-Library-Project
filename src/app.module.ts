4fe7af852a8701cf6b4a7877a1cb42ac8a71af82
aliSoltani-prog <alisoltaniprog@gmail.com>
2025-09-08 10:23:55 +0330

    adding the entities inside project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './books/entities/book.entity';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthorsModule } from './authors/authors.module';
+import { UsersModule } from './users/users.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule , TypeOrmModule.forRoot({
+    type : 'mysql' ,
+    host : 'loalhost',
+    port : 3306 ,
+    username : "aliSoltani" ,
+    password : "mysql",
+    database : "graphql-librarydb",
+    entities: [Book],
+    synchronize : true
+  }) , GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver : ApolloDriver, 
+    autoSchemaFile : "src/schema.gql"
+  }), AuthorsModule, UsersModule, ProfileModule  ],
   controllers: [AppController],
   providers: [AppService],
 })
